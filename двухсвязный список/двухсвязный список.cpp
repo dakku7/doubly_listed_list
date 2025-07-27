21cc7629a6eaa289d20027f9d19a31b4f9bd73b8
dakku <116499879+dakku7@users.noreply.github.com>
2025-07-27 21:28:57 +0200

    project files

---
+++ двухсвязный список/двухсвязный список.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+#include "listD.h"
+#include <array>
+
+int main(int argc, char* argv) {
+	std::array<int, 3> bbb;
+	auto iter = bbb.begin();
+
+	DoublLinkedList<int> arr;
+
+	arr.push_back(1);
+
+	arr.push_back(3);
+
+	arr.push_back(7); 
+	
+	arr.push_back(4);
+	
+	arr.push_back(10);
+	arr.Print();
+	std::cout << "\n";
+	
+	arr.fill(5, 3);
+	arr.Print();
+	
+	
+	return 0;
+}
